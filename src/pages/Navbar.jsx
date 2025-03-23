c1aeaf05dc829358d7cbe1496baf88e61da4e5aa
johel cueva <johel.cueva@gmail.com>
2025-03-22 20:43:50 -0500

    estilos

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import './Navbar.css'; // Importa el archivo CSS para estilos personalizados
+import './Navbar.css';
 
 function NavigationBar() {
+    const [expanded, setExpanded] = useState(false);
+
+    const closeMenu = () => setExpanded(false);
+
     return (
-        <Navbar bg="primary" variant="dark" expand="sm" className="mb-3 fixed-navbar">
+        <Navbar 
+            bg="primary" 
+            variant="dark" 
+            expand="sm" 
+            className="mb-3 fixed-navbar"
+            expanded={expanded}
+            onToggle={() => setExpanded(!expanded)}
+        >
             <Container>
                 <Navbar.Brand href="/">Contact Manager</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-nav" />
                 <Navbar.Collapse id="navbar-nav">
                     <Nav className="ms-auto">
-                        <NavLink to="/" className="nav-link">
+                        <NavLink 
+                            to="/" 
+                            className="nav-link" 
+                            onClick={closeMenu}
+                            end
+                        >
                             Agenda
                         </NavLink>
-                        <NavLink to="/create" className="nav-link">
+                        <NavLink 
+                            to="/create" 
+                            className="nav-link"
+                            onClick={closeMenu}
+                        >
                             Crear
                         </NavLink>
                     </Nav>
